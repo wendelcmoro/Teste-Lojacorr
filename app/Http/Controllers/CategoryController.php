1793f0aea2ec27390c0400e637e9fe08782bf802
Wendel Moro <wendelcmoro@gmail.com>
2024-07-27 22:52:21 -0300

    [Add] adicionando documentação com swagger

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,6 +9,86 @@ use App\Models\Category;
 
 class CategoryController extends Controller
 {
+     /**
+     * @OA\Post(
+     *     path="/api/category",
+     *     summary="Create a new category",
+     *     tags={"Categories"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             required={"name"},
+     *             @OA\Property(
+     *                 property="name",
+     *                 type="string",
+     *                 description="Category name"
+     *             ),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="success", type="boolean"),
+     *             @OA\Property(
+     *                  property="msg", 
+     *                  type="string", 
+     *                  example="Category created successfully"
+     *             ),
+     *             @OA\Property(
+     *                 property="category",
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="id",
+     *                     type="integer",
+     *                     description="Category ID"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string",
+     *                     description="Category name"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="subcategories",
+     *                     type="array",
+     *                     description="Subcategories",
+     *                     @OA\Items(
+     *                          @OA\Property(
+     *                              property="name",
+     *                              type="string",
+     *                              description="Subcategory name"
+     *                          ),
+     *                          @OA\Property(
+     *                              property="category_id",
+     *                              type="integer",
+     *                              description="Category id"
+     *                          )
+     *                     )
+     *                 ),
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal Server Error",
+     *         @OA\JsonContent(
+     *             @OA\Property(
+     *                 property="id", 
+     *                 type="array",
+     *                 @OA\Items(
+     *                      type="string",
+     *                      example="The name has already been taken."                     
+     *                 )
+     *             )     
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad request"
+     *     )
+     * )
+     */
     public function postCategory(Request $request) {
         $validator = Validator::make($request->all(), [
             'name' => 'required|unique:categories|max:255',
@@ -32,6 +112,72 @@ class CategoryController extends Controller
 		);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/categories",
+     *     summary="Show a category or a list of categories",
+     *     tags={"Categories"},
+     *     @OA\Parameter(
+     *         name="id",
+     *         in="query",
+     *         required=false,
+     *         @OA\Schema(type="integer"),
+     *         description="Category ID",
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="success", type="boolean"),
+     *             @OA\Property(
+     *                 property="categories",
+     *                 type="array",
+     *                 @OA\Items(
+     *                      @OA\Property(
+     *                          property="category",
+     *                          type="object",
+     *                          @OA\Property(
+     *                              property="id",
+     *                              type="integer",
+     *                              description="Category ID"
+     *                          ),
+     *                          @OA\Property(
+     *                              property="name",
+     *                              type="string",
+     *                              description="Category name"
+     *                          ),
+     *                          @OA\Property(
+     *                              property="subcategories",
+     *                              type="array",
+     *                              description="Subcategories",
+     *                              @OA\Items(
+     *                                  @OA\Property(
+     *                                      property="name",
+     *                                      type="string",
+     *                                      description="Subcategory name"
+     *                                  ),
+     *                                  @OA\Property(
+     *                                      property="category_id",
+     *                                      type="integer",
+     *                                      description="Category id"
+     *                                  )
+     *                              )
+     *                          )
+     *                      )
+     *                 )
+     *              )
+     *          )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal Server Error",
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad request"
+     *     )
+     * )
+     */
     public function getCategories(Request $request) {
         $validator = Validator::make($request->all(), [
             'id' => 'nullable|numeric',
@@ -57,6 +203,76 @@ class CategoryController extends Controller
 		);
     }
 
+    /**
+     * @OA\Delete(
+     *     path="/api/category",
+     *     summary="Delete a category",
+     *     tags={"Categories"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             type="object",
+     *             required={"name"},
+     *             @OA\Property(
+     *                 property="id",
+     *                 type="integer",
+     *                 description="Category ID"
+     *             ),
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent(
+     *             @OA\Property(property="success", type="boolean"),
+     *             @OA\Property(
+     *                  property="msg", 
+     *                  type="string", 
+     *                  example="Category deleted successfully"
+     *             ),
+     *             @OA\Property(
+     *                 property="category",
+     *                 type="object",
+     *                 @OA\Property(
+     *                     property="id",
+     *                     type="integer",
+     *                     description="Category ID"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="name",
+     *                     type="string",
+     *                     description="Category name"
+     *                 ),
+     *                 @OA\Property(
+     *                     property="subcategories",
+     *                     type="array",
+     *                     description="Subcategories",
+     *                     @OA\Items(
+     *                          @OA\Property(
+     *                              property="name",
+     *                              type="string",
+     *                              description="Subcategory name"
+     *                          ),
+     *                          @OA\Property(
+     *                              property="category_id",
+     *                              type="integer",
+     *                              description="Category id"
+     *                          )
+     *                     )
+     *                 ),
+     *             )
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=500,
+     *         description="Internal Server Error",
+     *     ),
+     *     @OA\Response(
+     *         response=400,
+     *         description="Bad request"
+     *     )
+     * )
+     */
     public function deleteCategory(Request $request) {
         $validator = Validator::make($request->all(), [
             'id' => 'nullable|numeric',
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+/**
+ * @OA\Info(title="Teste Lojacorr API", version="1")
+ */
+
 abstract class Controller
 {
     //
